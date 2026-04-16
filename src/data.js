c65b77c41d19b74f9326f678a850dc68f6da8ce3
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-04-16 22:39:48 +0800

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const jadual = {
+  Isnin: [
+    { subjek: "Bahasa Melayu", buku: ["Buku Teks BM", "Buku Latihan BM"] },
+    { subjek: "Matematik", buku: ["Buku Latihan Matematik"] },
+    { subjek: "Sains", buku: ["Buku Teks Sains"] }
+  ],
+  Selasa: [
+    { subjek: "Bahasa Inggeris", buku: ["Buku Teks BI"] },
+    { subjek: "Matematik", buku: ["Buku Teks Matematik"] },
+    { subjek: "Pendidikan Islam", buku: ["Buku PI"] }
+  ],
+  Rabu: [
+    { subjek: "Sains", buku: ["Buku Sains"] },
+    { subjek: "Bahasa Melayu", buku: ["Latihan BM"] },
+    { subjek: "Seni", buku: ["Buku Lukisan"] }
+  ],
+  Khamis: [
+    { subjek: "Matematik", buku: ["Buku Matematik"] },
+    { subjek: "Bahasa Inggeris", buku: ["Latihan BI"] },
+    { subjek: "PJ", buku: ["Pakaian Sukan"] }
+  ],
+  Jumaat: [
+    { subjek: "Pendidikan Islam", buku: ["Buku PI"] },
+    { subjek: "Bahasa Melayu", buku: ["Buku BM"] }
+  ]
+};
+
+export default jadual;
